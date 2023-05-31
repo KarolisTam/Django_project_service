e3a6a3c3f767dc1ce63bbd5179231e0e55fe4320
KarolisTam <130557721+KarolisTam@users.noreply.github.com>
2023-05-31 10:52:31 +0300

    Fixed admin panele for better view

--- autoservice/service/admin.py
+++ autoservice/service/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 from . import models
+from .models import OrderEntry
 # Register your models here.
 
-admin.site.register(models.Order)
-admin.site.register(models.Car)
+class OrderEntryInLine(admin.TabularInline):      # Pasiema oderentery lentele
+    model = OrderEntry
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'car', 'date')
+    inlines = [OrderEntryInLine]    # Prideda OderEntry lente po oder, kad galima butu prideti dar viena 
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'vin_code', 'licence_plate', 'model')
+    list_filter = ('model', 'customer')
+    search_fields = ('licence_plate', 'vin_code')
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+
+
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.Car, CarAdmin)
 admin.site.register(models.CarModel)
-admin.site.register(models.Service)
+admin.site.register(models.Service, ServiceAdmin)
 admin.site.register(models.OrderEntry)
