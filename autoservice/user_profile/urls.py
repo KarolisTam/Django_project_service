8f0338f5f68f6cbb9423e978ad92d2f29bf1ce14
KarolisTam <130557721+KarolisTam@users.noreply.github.com>
2023-06-08 09:58:33 +0300

    profile update function add

--- autoservice/user_profile/urls.py
+++ autoservice/user_profile/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
-    path('profile/<int:user_id>/', views.profile, name='profile')
+    path('profile/<int:user_id>/', views.profile, name='profile'),
+    path('profile/update/', views.profile_update, name="profile_update"),
 ]
